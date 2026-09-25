1360815acceb0886aa74483348631f6575abc9e2
anish9265 <anishyadav.creator@gmail.com>
2026-09-26 00:41:34 +0530

    Update cleanup-events.js

--- api/admin/cleanup-events.js
+++ api/admin/cleanup-events.js
@@ -320,11 +320,9 @@ export default async function handler(req, res) {
       error
     );
 
-    return res.status(500).json({
-      error:
-        error.message ||
-        "Something went wrong"
-    });
+return res.status(500).json({
+  error: "Something went wrong"
+});
 
   }
 
